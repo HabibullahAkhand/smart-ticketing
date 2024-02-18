138c7d7ac648d6d416a76536ebfd7339498e26af
habib39961592 <habib39961592@gmail.com>
2024-02-18 18:42:05 +0600

    maximum 4 ticket are bye

--- scripts/smart-ticketing.js
+++ scripts/smart-ticketing.js
@@ -21,5 +21,25 @@ function myFunction(elementId) {
     const updatedSet = currentSet - 1;
     setTextElementValueById('current-set', updatedSet);
 
-    
+    // calculation Table
+    calculationTable(innerText);
+
+    // disable after click
+    disableOnClick(innerText);
+
+    // seat booking counter
+    const bookingSetElement = document.getElementById('booking-seat');
+    const bookingSetText = bookingSetElement.innerText;
+    const bookingSet = parseInt(bookingSetText);
+    const updatedBookingSet = bookingSet + 1;
+    setTextElementValueById('booking-seat', updatedBookingSet);
+
+    // disable all onclick function
+    if (updatedBookingSet === 4) {
+        disableAllSeats();
+    }
+
+
 }
+
+
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,9 +1,52 @@
-function setBackgroundColorById(elementId){
+function setBackgroundColorById(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('bg-primary-color');
 }
 
-function setTextElementValueById(elementId, value){
+function setTextElementValueById(elementId, value) {
     const element = document.getElementById(elementId);
     element.innerText = value;
+}
+
+function calculationTable(setNumber) {
+    var ulElement = document.createElement('ul');
+    ulElement.classList.add('flex', 'justify-between', 'my-5');
+
+    var p1 = document.createElement('p');
+    p1.textContent = setNumber;
+    var p2 = document.createElement('p');
+    p2.textContent = 'Economoy';
+    var p3 = document.createElement('p');
+    p3.textContent = '550';
+
+    ulElement.appendChild(p1);
+    ulElement.appendChild(p2);
+    ulElement.appendChild(p3);
+    
+    const calculationTable = document.getElementById('calculation-table');
+    calculationTable.appendChild(ulElement);
+}
+
+function disableOnClick(elementId) {
+    var element = document.getElementById(elementId);
+    if (element) {
+        element.onclick = function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+            return false;
+        };
+        element.style.cursor = 'not-allowed';
+    } else {
+        console.error('Element with ID ' + elementId + ' not found.');
+    }
+}
+
+
+function disableAllSeats() {
+    const seatElements = document.querySelectorAll('.py-4');
+    seatElements.forEach(seat => {
+        seat.onclick = null;
+        seat.style.cursor = 'not-allowed';
+        seat.style.opacity = 0.5;
+    });
 }
