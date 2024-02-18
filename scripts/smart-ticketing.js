1ed31dcb6df8f850ecc63de802dde5009686568e
habib39961592 <habib39961592@gmail.com>
2024-02-18 17:48:49 +0600

    use BG color and current update set number

--- scripts/smart-ticketing.js
+++ scripts/smart-ticketing.js
@@ -1,1 +1,25 @@
-console.log('smart-ticketing');
+function scrollToElement(elementId) {
+    let element = document.getElementById(elementId);
+    if (element) {
+        var offsetTop = element.getBoundingClientRect().top + window.pageYOffset;
+        window.scrollTo({
+            top: offsetTop,
+            behavior: 'smooth'
+        });
+    }
+}
+
+function myFunction(elementId) {
+    // bg color change
+    const innerText = elementId.innerText;
+    setBackgroundColorById(innerText);
+
+    // update current set
+    const currentSetElement = document.getElementById('current-set');
+    const currentSetText = currentSetElement.innerText;
+    const currentSet = parseInt(currentSetText);
+    const updatedSet = currentSet - 1;
+    setTextElementValueById('current-set', updatedSet);
+
+    
+}
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,1 +1,9 @@
-console.log('utility');
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-primary-color');
+}
+
+function setTextElementValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
